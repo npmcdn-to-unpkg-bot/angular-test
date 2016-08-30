e46692df5182db3b291a66d550eccdd10d07f5ac
barmanRoman <romanzanko93@gmail.com>
2016-08-30 21:25:31 +0300

    Change 2

--- controller/QuestionController.js
+++ controller/QuestionController.js
@@ -1,29 +1,31 @@
-﻿var myApp = angular.module("myApp",[]);
-myApp.controller('QuestionCtrl', function ($scope) {
+﻿var myApp = angular.module("myApp", []);
+
+myApp.factory("dataService", function () {
+    return {
+        question: {
+            text: 'Какой js-фреймворк лучше использовать?',
+            author: 'Иван Иванов',
+            date: '20/10/2013',
+            answers:
+            [{
+                text: 'AngularJS!',
+                author: 'Вова Сидоров',
+                date: '20/10/2013',
+                rate: 2
+            }, {
+                text: 'AngularJS лучше всех',
+                author: 'Олег Кузнецов',
+                date: '21/10/2013',
+                rate: 3
+            }]
+        }
+    };
+});
+
+myApp.controller('QuestionCtrl', function ($scope, dataService) {
 
     $scope.sortparam = 'rate';
-    $scope.question = {
-        text: 'Какой js-фреймворк лучше использовать?',
-        author: 'Иван Иванов',
-        date: '20/10/2013',
-        answers:
-        [{
-            text: 'AngularJS!',
-            author: 'Вова Сидоров',
-            date: '20/10/2013',
-            rate: 2
-        }, {
-            text: 'AngularJS лучше всех',
-            author: 'Олег Кузнецов',
-            date: '20/10/2013',
-            rate: 0
-        }, {
-            text: 'Я бы использовал knockout',
-            author: 'Неизвестный',
-            date: '21/10/2013',
-            rate: 0
-        }]
-    };
+    $scope.question = dataService.question;
 
     $scope.voteUp = function (answer) {
         answer.rate++;
@@ -37,6 +39,27 @@ myApp.controller('QuestionCtrl', function ($scope) {
         $scope.questColorClass = e.type == "mouseover" ? "questselectedcolor" : "questcolor";
     }
 });
+
+myApp.controller('Question2Ctrl',
+    function QuestionController($scope, $http) {
+
+        $scope.loaded = false;
+
+        $scope.load = function () {
+            $http.get('data.json').
+             success(function (data) {
+                 $scope.question = data.question;
+                 $scope.loaded = true;
+             })
+        };
+        $scope.voteUp = function (answer) {
+            answer.rate++;
+        };
+        $scope.voteDown = function (answer) {
+            answer.rate--;
+        };
+    }
+)
 
 myApp.controller("AnswerCtrl", function ($scope) {
     $scope.save = function (answer, answerForm) {
