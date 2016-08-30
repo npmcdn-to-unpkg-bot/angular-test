017af85342f20146b8ca1cecee4970d502fc6f7f
Roman Zanko <romanzanko93@gmail.com>
2016-08-30 16:12:09 +0300

    Change 1

---
+++ controller/answerList.js
@@ -0,0 +1,10 @@
+﻿myApp.directive("answerList", function () {
+    return {
+        link: function (scope, element, attrs) {
+
+            scope.data = scope[attrs["answerList"] || attrs["source"]];
+        },
+        restrict: "A",
+        templateUrl: "templates/answersTemplate.html"
+    }
+});
